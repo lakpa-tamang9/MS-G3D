42e95b17429f45fb4c4a6df9c862e00d60779b16
lakpa-tamang9 <Ld.tamang25@gmail.com>
2022-08-24 13:33:34 +0900

    feat: Add index mapper for medipipe

--- graph/mediapipe.py
+++ graph/mediapipe.py
@@ -25,15 +25,24 @@ joint_index = {0 : "Nose", 11 : "LShoulder", 12 : "RShoulder", 13 : "LElbow",
 
 num_node = 13
 indexes = list(joint_index.keys())
-self_link = [(i , i) for i in indexes]
 
-inward = [(16, 14), (14, 12), (15, 13), (13, 11), (27, 25), (25, 23), 
-          (28, 26), (26, 24), (23, 11), (24, 12), (24, 23), (12, 11), (0, 12), (0, 11)]
+# Mapping the selected pose landmarks in a sequential manner
+index_mapper = {0 :0, 1: 11, 2 : 12, 3 : 13, 4 : 14, 5 : 15, 
+                6: 16, 7 : 23, 8 : 24, 9 : 25, 10 : 26, 11 : 27, 12 : 28}
+
+self_link = [(i, i) for i in range(num_node)]
+# self_link = [(i , i) for i in indexes]
+
+# inward = [(16, 14), (14, 12), (15, 13), (13, 11), (27, 25), (25, 23), 
+#           (28, 26), (26, 24), (23, 11), (24, 12), (24, 23), (12, 11), (0, 12), (0, 11)]
+
+inward = [(6, 4), (4, 2), (5, 3), (3, 1), (11, 9), (9, 7), (12, 10), 
+          (10, 8), (7, 1), (8, 2), (8, 7), (2, 1), (0, 2), (0, 1)]
 
 outward = [(j, i) for (i, j) in inward]
 
 neighbor = inward + outward
-
+print(neighbor)
 
 class AdjMatrixGraph:
     def __init__(self, *args, **kwargs):
